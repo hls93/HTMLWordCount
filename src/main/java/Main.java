9f711f08422618c3a7faff3ca0a6015ba9bc2997
hls93 <hlshaw3@ncsu.edu>
2017-12-06 11:08:17 -0500

    adding exception for invalid url

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@ import org.jsoup.nodes.Document;
 
 
 import java.io.IOException;
+import java.net.UnknownHostException;
 import java.util.*;
 
 public class Main {
@@ -10,45 +11,56 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         Scanner scanner = new Scanner(System.in);
+        boolean isValid = false;
 
         //using Jsoup, I am connecting to the url and parsing through the html after asking for user input
-        System.out.println("Please enter a URL");
-        String webPage = scanner.nextLine();
+        do {
+            System.out.println("Please enter a URL");
+            String webPage = scanner.nextLine();
+            try {
 
-        String html = Jsoup.connect(webPage).get().html();
+                String html = Jsoup.connect(webPage).get().html();
 
-        Document doc = Jsoup.parse(String.valueOf(html));
+                Document doc = Jsoup.parse(String.valueOf(html));
 
-        //extracting the text from the body of the web-page
-        String text = doc.body().text();
+                //extracting the text from the body of the web-page
+                String text = doc.body().text();
 
-        //creating a new list to show the words on the web-page and sorting it
-        List<String> list = Arrays.asList(text.split(" "));
-        Collections.sort(list);
+                //creating a new list to show the words on the web-page and sorting it
+                List<String> list = Arrays.asList(text.split(" "));
+                Collections.sort(list);
 
-        //creating a hashMap to store the word and its frequency
-        HashMap<String, Integer> map = new HashMap<>();
+                //creating a hashMap to store the word and its frequency
+                HashMap<String, Integer> map = new HashMap<>();
 
-        //looping over the list. If the word is found multiple times (contains key) we are adding 1 to the value of count, then adding it to the map. If not, count equals one.
-        for (int i = 0; i < list.size(); i++) {
-            String s = list.get(i).toLowerCase();
-            int count;
-            if (map.containsKey(s)) {
-                count = map.get(s) + 1;
-            } else {
-                count = 1;
+                //looping over the list. If the word is found multiple times (contains key) we are adding 1 to the value of count, then adding it to the map. If not, count equals one.
+                for (int i = 0; i < list.size(); i++) {
+                    String s = list.get(i).toLowerCase();
+                    int count;
+                    if (map.containsKey(s)) {
+                        count = map.get(s) + 1;
+                    } else {
+                        count = 1;
+                    }
+
+                    map.put(s, count);
+
+                }
+                //now sorting the list by frequency and limiting it to print the top 25
+                map.entrySet()
+                        .stream()
+                        .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
+                        .limit(25)
+                        .forEach(System.out::println);
+
+            } catch (IllegalArgumentException e) {
+                System.out.println("Url is not valid. Please try again");
             }
-
-            map.put(s, count);
-
         }
-        //now sorting the list by frequency and limiting it to print the top 25
-        map
-                .entrySet()
-                .stream()
-                .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
-                .limit(25)
-                .forEach(System.out::println);
+        while(!isValid);
+        return;
+
+
 
     }
 }
